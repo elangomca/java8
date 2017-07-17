5d2d954cefe1c427dcf18f18607825cc085c05dc
Elango Maragatham <elango.maragatham@autodesk.com>
2017-07-17 13:44:38 +0800

    Initial commit

---
+++ src/main/java/com/elango/Java8Helper.java
@@ -0,0 +1,13 @@
+package com.elango;
+
+/**
+ * Created by maragae on 7/17/2017.
+ */
+public class Java8Helper {
+    public static void test1(boolean flag) {
+        if(flag)
+            System.out.println(1);
+        else
+            System.out.println(0);
+    }
+}
---
+++ src/test/java/com/elango/Java8HelperTest.java
@@ -0,0 +1,12 @@
+package com.elango;
+
+import org.junit.Test;
+
+public class Java8HelperTest {
+    @Test
+    public void testTest1() {
+        Java8Helper helper = new Java8Helper();
+        helper.test1(true);
+        helper.test1(false);
+    }
+}
